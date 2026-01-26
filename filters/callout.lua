031c9cadf7b450b67ae13316efac9ea74cae6066
zcysxy <zcysxy.wyzdz@gmail.com>
2026-01-25 20:01:19 -0500

    mod callout.lua

--- filters/callout.lua
+++ filters/callout.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 --[[
   Pandoc filter for Obsidian callouts
 
@@ -14,115 +15,117 @@
   - definition, def
   - assumption, asmp
   - abstract
+	- proof, pf
+	- fact
+	- proposition, prop
 
   By github.com/zcysxy
 --]]
 
--- local logging = require "logging"
+-- Transform callout type to titlecase
+local function titlecase(str)
+	return str:sub(1, 1):upper() .. str:sub(2, -1)
+end
+
+-- Check if the callout type is in a list
+local function has_value(tab, val)
+	for _, value in ipairs(tab) do
+		if value == val then
+			return true
+		end
+	end
+	return false
+end
+
+-- Parse callout title
+local function get_raw_tex(para)
+	para.content = para.content:walk {
+		Math = function(el) return "$" .. el.text .. "$" end,
+		Emph = function(el) return "\\textit{" .. pandoc.utils.stringify(el.content) .. "}" end,
+		Strong = function(el) return "\\textbf{" .. pandoc.utils.stringify(el.content) .. "}" end,
+		Code = function(el) return "\\textit{" .. el.text .. "}" end
+	}
+	return para
+end
 
 function BlockQuote(elem)
-  -- Transform callout type to titlecase
-  local function titlecase(str)
-    return str:sub(1, 1):upper() .. str:sub(2, -1)
-  end
-
-  -- Check if the callout type is in a list
-  local function has_value(tab, val)
-    for index, value in ipairs(tab) do
-      if value == val then
-        return true
-      end
-    end
-    return false
-  end
-
-  -- Parse callout title
-  local function get_raw_tex(para)
-    para.content = para.content:walk {
-      Math = function(el) return "$" .. el.text .. "$" end,
-      Emph = function(el) return "\\textit{" .. pandoc.utils.stringify(el.content) .. "}" end,
-      Strong = function(el) return "\\textbf{" .. pandoc.utils.stringify(el.content) .. "}" end,
-      Code = function(el) return "\\textit{" .. el.text .. "}" end
-    }
-    return para
-  end
-
-  -- callout_type = elem.content[1].content[1].text:match("^%[%!([a-z0-9%-]*)%]")
-  callout_type = pandoc.utils.stringify(elem.content[1].content[1])
+  local callout_type = pandoc.utils.stringify(elem.content[1].content[1])
   callout_type = callout_type:match("^%[%!([a-z0-9%-]*)%]")
 
   -- Plain blockquote
   if not callout_type then
     return elem
-  else
-    -- Special callout types
-    if has_value({ "rmk", "remark" }, callout_type) then
-      block_color = "red"
-      callout_type = "Remark"
-    elseif has_value({ "qn", "question" }, callout_type) then
-      block_color = "orange"
-      callout_type = "Question"
-    elseif has_value({ "impt", "important", "def", "definition" }, callout_type) then
-      block_color = "yellow!80!white"
-      callout_type = "Important"
-    elseif has_value({ "ex", "example" }, callout_type) then
-      block_color = "green"
-      callout_type = "Example"
-    elseif has_value({ "meta", "x" }, callout_type) then
-      return {}
-    else
-      block_color = "cyan!70!white"
-    end
-    callout_type = titlecase(callout_type)
+	end
 
-    -- Get callout title
-    callout_title = pandoc.utils.stringify(get_raw_tex(elem.content[1]))
-    callout_title = callout_title:gsub("^%[%!([a-z0-9%-]*)%][%+%-]? ?", "")
-    title, id = callout_title:match("(.*) *^([0-9a-zA-Z-]*)")
-    if title then
-      callout_title = title
-    end
+	-- Special callout types
+	local block_color
+	if has_value({ "rmk", "remark" }, callout_type) then
+		block_color = "red!80!white"
+		callout_type = "Remark"
+	elseif has_value({ "qn", "question" }, callout_type) then
+		block_color = "orange!80!white"
+		callout_type = "Question"
+	elseif has_value({ "impt", "important", "def", "definition" }, callout_type) then
+		block_color = "yellow!80!white"
+		callout_type = "Important"
+	elseif has_value({ "ex", "example" }, callout_type) then
+		block_color = "green"
+		callout_type = "Example"
+	elseif has_value({ "meta", "x" }, callout_type) then
+		return {}
+	else
+		block_color = "cyan!70!white"
+	end
+	callout_type = titlecase(callout_type)
 
-    table.remove(elem.content, 1)
-    title_only = next(elem.content) == nil
+	-- Get callout title
+	local callout_title = pandoc.utils.stringify(get_raw_tex(elem.content[1]))
+	callout_title = callout_title:gsub("^%[%!([a-z0-9%-]*)%][%+%-]? ?", "")
+	local title, id = callout_title:match("(.*) *^([0-9a-zA-Z-]*)$")
+	if title then
+		callout_title = title
+	end
 
-    -- Special callout types
-    if callout_type == "Abstract" then
-      latex_begin_string = "\\begin{abstract}"
-      latex_end_string = "\\end{abstract}"
-    elseif has_value({ "Assumption", "Asmp" }, callout_type) then
-      latex_begin_string = "\\begin{assumption}[" .. callout_title .. "]"
-      latex_end_string = "\\end{assumption}"
-    elseif has_value({ "Definition", "Def" }, callout_type) then
-      latex_begin_string = "\\begin{definition}[" .. callout_title .. "]"
-      latex_end_string = "\\end{definition}"
-    elseif has_value({ "Theorem", "Thm" }, callout_type) then
-      latex_begin_string = "\\begin{theorem}[" .. callout_title .. "]"
-      latex_end_string = "\\end{theorem}"
-    elseif has_value({ "Fact" }, callout_type) then
-      latex_begin_string = "\\begin{fact}[" .. callout_title .. "]"
-      latex_end_string = "\\end{fact}"
-    elseif has_value({ "Prop", "Proposition" }, callout_type) then
-      latex_begin_string = "\\begin{proposition}[" .. callout_title .. "]"
-      latex_end_string = "\\end{proposition}"
-    elseif has_value({ "Pf", "Proof" }, callout_type) then
-      latex_begin_string = "\\begin{proof}"
-      latex_end_string = "\\end{proof}"
-    else
-      if title_only then
-        latex_begin_string_template =
-        "\\begin{tcolorbox}[noparskip,breakable,coltitle=black,colback=%s!5!white,colframe=%s,fonttitle=\\bfseries] %s"
-      else
-        latex_begin_string_template =
-        "\\begin{tcolorbox}[noparskip,breakable,coltitle=black,colback=%s!5!white,colframe=%s,title=%s,fonttitle=\\bfseries]"
-      end
-      latex_end_string = "\\end{tcolorbox}"
-      if callout_title:match('[^%s]') then
-        latex_begin_string = string.format(latex_begin_string_template, block_color, block_color, callout_title)
-      else -- use type as title
-        latex_begin_string = string.format(latex_begin_string_template, block_color, block_color, callout_type)
-      end
-    end
+	table.remove(elem.content, 1)
+	local title_only = next(elem.content) == nil
+
+	-- Special callout types
+	local latex_begin_string, latex_end_string, latex_begin_string_template
+	if callout_type == "Abstract" then
+		latex_begin_string = "\\begin{abstract}"
+		latex_end_string = "\\end{abstract}"
+	elseif has_value({ "Assumption", "Asmp" }, callout_type) then
+		latex_begin_string = "\\begin{assumption}[" .. callout_title .. "]"
+		latex_end_string = "\\end{assumption}"
+	elseif has_value({ "Definition", "Def" }, callout_type) then
+		latex_begin_string = "\\begin{definition}[" .. callout_title .. "]"
+		latex_end_string = "\\end{definition}"
+	elseif has_value({ "Theorem", "Thm" }, callout_type) then
+		latex_begin_string = "\\begin{theorem}[" .. callout_title .. "]"
+		latex_end_string = "\\end{theorem}"
+	elseif has_value({ "Fact" }, callout_type) then
+		latex_begin_string = "\\begin{fact}[" .. callout_title .. "]"
+		latex_end_string = "\\end{fact}"
+	elseif has_value({ "Prop", "Proposition" }, callout_type) then
+		latex_begin_string = "\\begin{proposition}[" .. callout_title .. "]"
+		latex_end_string = "\\end{proposition}"
+	elseif has_value({ "Pf", "Proof" }, callout_type) then
+		latex_begin_string = "\\begin{proof}"
+		latex_end_string = "\\end{proof}"
+	else
+		if title_only then
+			latex_begin_string_template =
+			"\\begin{tcolorbox}[noparskip,breakable,coltitle=black,colback=%s!5!white,colframe=%s,fonttitle=\\bfseries] %s"
+		else
+			latex_begin_string_template =
+			"\\begin{tcolorbox}[noparskip,breakable,coltitle=black,colback=%s!5!white,colframe=%s,title=%s,fonttitle=\\bfseries]"
+		end
+		latex_end_string = "\\end{tcolorbox}"
+		if callout_title:match('[^%s]') then
+			latex_begin_string = string.format(latex_begin_string_template, block_color, block_color, callout_title)
+		else -- use type as title
+			latex_begin_string = string.format(latex_begin_string_template, block_color, block_color, callout_type)
+		end
 
     -- Use Obsidian ID as label
     if id then
