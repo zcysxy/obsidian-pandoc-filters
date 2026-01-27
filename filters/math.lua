6fb89136676ec4630f7a0d576aa22831b180fe26
zcysxy <zcysxy.wyzdz@gmail.com>
2026-01-27 03:26:24 -0500

    mod math.lua

--- filters/math.lua
+++ filters/math.lua
@@ -1,10 +1,14 @@
-
--- local logging = require 'logging'
+-- [[
+-- Transform mathjax/katex non-compatible syntax for LaTeX.
+--
+-- By github.com/zcysxy
+-- ]]
 
 function Math (el)
     el.text = el.text:gsub("\\mathbb{1}", "\\mathbbm{1}")
     el.text = el.text:gsub('%%\\label', '\\label')
     el.text = el.text:gsub('\\tag{.-}%%', '')
+    el.text = el.text:gsub('%%%%', '')
     return el
 end
 
